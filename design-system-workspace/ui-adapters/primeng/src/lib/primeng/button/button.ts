4dbc1b39eea20b75f6f9bd66c2bcb716ebd28318
Tharaka_Madhusanka <tharakalearn@gmail.com>
2026-02-10 21:18:25 +0800

    Added PrimeNg UI Adapter

---
+++ design-system-workspace/ui-adapters/primeng/src/lib/primeng/button/button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'primeng-primeng',
+  imports: [],
+  templateUrl: './primeng.html',
+  styleUrl: './primeng.scss',
+})
+export class PrimengButton {}
